de443ad5a1c41ea681e46cb71a48a337ef6875b0
Nikita <nothanks000@gmail.com>
2017-02-02 20:01:32 +0300

    Add files via upload
    starting new game

---
+++ GameSnake.java
@@ -0,0 +1,12 @@
+package com.example.snake;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+
+public class GameSnake {
+
+    public static void main(String[] args) {
+    }
+}
